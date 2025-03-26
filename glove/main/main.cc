182eb6d6d440e6b6e18d9c43a60ebf69360f13a1
jarkad <jarkad@jarkad.net.eu.org>
2025-03-26 13:49:17 +0100

    draw animation

--- glove/main/main.cc
+++ glove/main/main.cc
@@ -1,14 +1,28 @@
 #include <Arduino.h>
+#include <TFT_eSPI.h>
+
+#include "heart.h"
+
+TFT_eSPI tft;
 
 static char const *TAG = "main";
 
 extern "C" void app_main() {
 	ESP_LOGI(TAG, "Hello world");
 
+	tft.init();
+
 	for (;;) {
+		for (int i = 0; i < heart_frames; i++) {
+			tft.pushImage((TFT_WIDTH - heart_width) / 2,
+				      (TFT_HEIGHT - heart_height) / 2,
+				      heart_width, heart_height,
+				      &heart[i * heart_width * heart_height]);
+
+			vTaskDelay(pdMS_TO_TICKS(50));
+		}
+
 		ESP_LOGI(TAG, "tick");
-		vTaskDelay(pdMS_TO_TICKS(500));
-		ESP_LOGI(TAG, "tock");
-		vTaskDelay(pdMS_TO_TICKS(500));
+		vTaskDelay(pdMS_TO_TICKS(50));
 	}
 }
