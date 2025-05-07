9a11bf7cad53c12d89031b94b14b8656448e058a
549531 <180322397+549531@users.noreply.github.com>
2025-05-07 16:42:18 +0200

    fmt: reformat web

--- Pint/src/components/logPage.vue
+++ Pint/src/components/logPage.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import {ref, watchEffect} from 'vue'
+import { ref, watchEffect } from 'vue'
 
 defineProps<{ msg: string }>()
 
@@ -20,23 +20,20 @@ function checkLogin() {
 }
 
 watchEffect(() => {
-  console.log(device_id.value);
+  console.log(device_id.value)
 })
-
-
 </script>
 
 <template>
-  <div class="flex items-center justify-center min-h-screen ">
+  <div class="flex items-center justify-center min-h-screen">
     <input
-        class="border border-black-300 rounded-full px-4 shadow block m-auto"
-
-        :placeholder="isFocused ? ' ':'login with device id'"
-        v-model="device_id"
-        @focus="isFocused = true"
-        @blur="isFocused = false"
-        @keydown.enter="checkLogin"
-        style="height: 50px; width: 450px;"
+      class="border border-black-300 rounded-full px-4 shadow block m-auto"
+      :placeholder="isFocused ? ' ' : 'login with device id'"
+      v-model="device_id"
+      @focus="isFocused = true"
+      @blur="isFocused = false"
+      @keydown.enter="checkLogin"
+      style="height: 50px; width: 450px"
     />
   </div>
 </template>
