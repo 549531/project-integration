4631418fac30c70aa46ca7c2bc7e2bd72e1d001c
Obidkhon Akhmadkhonov <obbteam12@gmail.com>
2025-05-21 20:03:08 +0200

    created fft class, connected mpu, testing fft with generated sinus wave

--- glove/main/main.cc
+++ glove/main/main.cc
@@ -1,8 +1,28 @@
+#include <Adafruit_MPU6050.h>
 #include <Arduino.h>
 #include <TFT_eSPI.h>
+#include <arduinoFFT.h>
 #include <lvgl.h>
 
 #include "button.hh"
+#include "fft.hh"
+#include "globals.hh"
+
+// MPU 6050
+Adafruit_MPU6050 mpu;
+
+// global fft object
+static fft g_fft;
+
+void initMPU() {
+	if (!mpu.begin()) {
+		Serial.println("MPU6050 not found");
+		while (true) delay(10);
+	}
+	mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+	mpu.setGyroRange(MPU6050_RANGE_250_DEG);     // best LSB resolution
+	mpu.setFilterBandwidth(MPU6050_BAND_44_HZ);  // stop >64 Hz aliasing
+}
 
 static char const *TAG = "main";
 
@@ -58,7 +78,11 @@ static void btn2_click(void *arg) {
 }
 
 extern "C" void app_main() {
+	Serial.begin(115200);
 	ESP_LOGI(TAG, "boot done");
+
+	initMPU();
+	ESP_LOGI(TAG, "mpu config done");
 
 	buttons_init();
 	ESP_LOGI(TAG, "buttons config done");
@@ -94,6 +118,7 @@ extern "C" void app_main() {
 
 	lv_timer_create(chart_update, 100, chart);
 	lv_timer_create(buttons_update, 33, chart);
+	lv_timer_create(fft::timer_cb, 1000 / fft::FS, &g_fft);
 
 	ESP_LOGI(TAG, "lvgl config done");
 
