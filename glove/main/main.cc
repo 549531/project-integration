cbc7cbecb8a9279279b6a0a5fe9c0abb1ca66bc8
549531 <180322397+549531@users.noreply.github.com>
2025-05-25 20:45:31 +0200

    glove: move buttons handling to buttons.cc

--- glove/main/main.cc
+++ glove/main/main.cc
@@ -2,30 +2,11 @@
 #include <TFT_eSPI.h>
 #include <lvgl.h>
 
-#include "button.hh"
+#include "buttons.hh"
 
 static char const *TAG = "main";
 
 uint32_t my_lv_draw_buf[TFT_WIDTH * TFT_HEIGHT / 4];
-
-static void btn1_click(void *);
-static void btn2_click(void *);
-
-static button_t buttons[]{
-    {GPIO_NUM_0, btn1_click},
-    {GPIO_NUM_35, btn2_click},
-};
-
-static void buttons_init() {
-	gpio_config_t buttons_gpio_config{
-	    .mode = GPIO_MODE_INPUT,
-	    .pull_up_en = GPIO_PULLUP_ENABLE,
-	};
-	for (auto &btn : buttons) {
-		buttons_gpio_config.pin_bit_mask |= 1llu << btn.m_pin;
-	}
-	ESP_ERROR_CHECK(gpio_config(&buttons_gpio_config));
-}
 
 static uint32_t my_lv_tick_get() { return millis(); }
 
@@ -36,32 +17,8 @@ static void chart_update(lv_timer_t *t) {
 	lv_chart_refresh(chart);
 }
 
-static void buttons_update(lv_timer_t *t) {
-	void *arg = lv_timer_get_user_data(t);
-	for (auto &btn : buttons) {
-		btn.update(arg);
-	}
-}
-
-static void btn1_click(void *arg) {
-	auto chart = (lv_obj_t *)arg;
-	auto series = lv_chart_get_series_next(chart, nullptr);
-	lv_chart_set_series_color(chart, series,
-				  lv_palette_main(LV_PALETTE_BLUE));
-}
-
-static void btn2_click(void *arg) {
-	auto chart = (lv_obj_t *)arg;
-	auto series = lv_chart_get_series_next(chart, nullptr);
-	lv_chart_set_series_color(chart, series,
-				  lv_palette_main(LV_PALETTE_GREEN));
-}
-
 extern "C" void app_main() {
 	ESP_LOGI(TAG, "boot done");
-
-	buttons_init();
-	ESP_LOGI(TAG, "buttons config done");
 
 	lv_init();
 	lv_tick_set_cb(my_lv_tick_get);
@@ -96,6 +53,9 @@ extern "C" void app_main() {
 	lv_timer_create(buttons_update, 33, chart);
 
 	ESP_LOGI(TAG, "lvgl config done");
+
+	buttons_init(chart);
+	ESP_LOGI(TAG, "buttons config done");
 
 	for (;;) {
 		uint32_t delay = lv_timer_handler();
