b0d5688f135ac532445af417ad06a179831caeb5
549531 <180322397+549531@users.noreply.github.com>
2025-05-26 14:01:58 +0200

    api: add streaming endpoint

--- api/pint/urls.py
+++ api/pint/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("devices/<int:device_id>/amplitude/", views.amplitude, name="amplitude"),
+    path(
+        "devices/<int:device_id>/amplitude/live/",
+        views.amplitude_live,
+        name="amplitude_live",
+    ),
 ]
--- api/pint/views.py
+++ api/pint/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, StreamingHttpResponse
 from random import randint
+import json
+import nats
 
 # Create your views here.
 
@@ -11,3 +13,41 @@ def index(request):
 def amplitude(request, device_id):
     values = [randint(0, 100) for _ in range(500)]
     return JsonResponse({"values": values})
+
+
+async def amplitude_live(request, device_id):
+    async def response():
+        nc = await nats.connect(
+            servers="tls://jarkad.net.eu.org",
+            tls_handshake_first=True,
+            user="pint",
+            password="pint",
+        )
+        js = nc.jetstream()
+        conf = nats.js.api.ConsumerConfig(ack_policy=nats.js.api.AckPolicy.NONE)
+        sub = await js.pull_subscribe(f"devices.{device_id}.amplitude", config=conf)
+
+        try:
+            while msgs := await sub.fetch(batch=100):
+                res = {
+                    "data": [
+                        {
+                            "time": int(msg.metadata.timestamp.timestamp() * 1000),
+                            "value": int(msg.data),
+                        }
+                        for msg in msgs
+                    ]
+                }
+                yield "data: "
+                yield json.dumps(res)
+                yield "\n\n"
+        except TimeoutError:
+            yield "\n\n"
+
+        await nc.close()
+
+    return StreamingHttpResponse(
+        response(),
+        content_type="text/event-stream",
+        headers={"Cache-Control": "no-cache"},
+    )
