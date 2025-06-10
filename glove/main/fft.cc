e5921894e80138b0a35c45f80f7ea4b5e593de9f
Obidkhon Akhmadkhonov <obbteam12@gmail.com>
2025-06-10 13:50:06 +0200

    glove: changed inv amplitude range for a motor from -1500 to 1500

--- glove/main/fft.cc
+++ glove/main/fft.cc
@@ -43,7 +43,9 @@ void fft::invert_signal(Network *net) {
 	float invSample = ampDrive * sinf(phaseAcc + phaseInv);
 	Serial.print(F(">Inv:"));
 	Serial.println(invSample);
-	servo.write(map(invSample * 10000, -3000, 3000, 0, 180));
+	const float range = 1.5;
+	const int digits = 1000;
+	servo.write(map(invSample * digits, -range*digits, range*digits, 0, 180));
 	net->push(invSample, "devices/12345678/inverted");
 }
 
