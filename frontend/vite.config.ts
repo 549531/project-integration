b4457cf45fbef97dd2354db6d881092c36a00b71
549531 <180322397+549531@users.noreply.github.com>
2025-06-10 13:14:11 +0200

    [LO]: frontend: use local api

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'https://project-integration.foo.ng',
+      '/api': 'http://127.0.0.1:2948',
     },
   },
   plugins: [vue(), tailwindcss()],
